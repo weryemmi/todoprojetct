6de884aa9ceb98597959635adc8d7b3da7d8b0b6
marwa <weryemmi.marwa@gmail.com>
2023-06-01 10:13:57 +0100

    mon premier commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Banner from './Banner'
+import Cart from './Cart'
+import Body from './Body'
+function App() {
+	return (
+		<div>
+			<Banner />
+			<Cart />
+			<Body />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/Body.js
@@ -0,0 +1,34 @@
+import '../styles/Body.css'
+ 
+// Example of a data array that
+// you might receive from an API
+const data = [
+    { name: "Anom", age: 19, gender: "Male" },
+    { name: "Megha", age: 19, gender: "Female" },
+    { name: "Subham", age: 25, gender: "Male" },
+]
+ 
+function Body() {
+    return (
+        <div className="App">
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Gender</th>
+                </tr>
+                {data.map((val, key) => {
+                    return (
+                        <tr key={key}>
+                            <td>{val.name}</td>
+                            <td>{val.age}</td>
+                            <td>{val.gender}</td>
+                        </tr>
+                    )
+                })}
+            </table>
+        </div>
+    );
+}
+ 
+export default Body;
